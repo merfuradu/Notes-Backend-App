fbe9e227a3624cfffe16b8dad9c1272ffe059b91
radu.merfu <radu.merfu@kdu.ro>
2025-01-20 23:36:05 +0200

    Changed Delete method.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -31,12 +31,12 @@ router.delete('/:id', async (req, res) => {
     try {
         const result = await pool.query('DELETE FROM "Note" WHERE id = $1', [id]);
         if (result.rowCount === 0) {
-            return res.status(404).send('Note not found');
+            return res.status(404).json( {message : "Note not found"});
         }
-        res.status(200).send('Note deleted');
+        res.status(200).json({message : "Deleted succesfully"});
     } catch (error) {
         console.error(error.message);
-        res.status(500).send('Server error');
+        res.status(500).json({message :"Server error" });
     }
 });
 
